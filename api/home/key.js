0b709221b52ee710645990f291ebec0d71d7410f
= <=>
2024-08-21 14:27:56 +0200

    add db

--- api/home/key.js
+++ api/home/key.js
@@ -1,35 +1,35 @@
 const { initializeApp } = require('firebase/app');
 const { getFirestore, doc, setDoc } = require('firebase/firestore');
 
-const myKey = process.env.apiKey;
-
-
+// Your Firebase configuration
 const firebaseConfig = {
-    apiKey: myKey,
+    apiKey: process.env.apiKey,
     authDomain: "aisturdyhelp.firebaseapp.com",
     projectId: "aisturdyhelp",
     storageBucket: "aisturdyhelp.appspot.com",
     messagingSenderId: "309870602306",
     appId: "1:309870602306:web:2273af72ef06e750c0a5db",
     measurementId: "G-L12NERV1JR"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  
-  
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
 
 export default async function handler(req, res) {
+    try {
+        // Add a new document in collection "cities"
+        await setDoc(doc(db, "cities", "LA"), {
+            name: "Los Angeles",
+            state: "CA",
+            country: "USA"
+        });
 
-
-
-// Add a new document in collection "cities"
-await setDoc(doc(db, "cities", "LA"), {
-  name: "Los Angeles",
-  state: "CA",
-  country: "USA"
-});
-
-
+        res.status(200).json({ success: true });
+    } catch (error) {
+        console.error("Error writing document: ", error);
+        res.status(500).json({ success: false, message: "Failed to write document", error });
+    }
 }
