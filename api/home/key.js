0061f114871fb5617136b226dc5f665c1883f212
= <=>
2024-08-21 14:23:22 +0200

    log

--- api/home/key.js
+++ api/home/key.js
@@ -1,14 +1,36 @@
+const { initializeApp } = require('firebase/app');
+const { getFirestore, doc, setDoc } = require('firebase/firestore');
+
 const myKey = process.env.apiKey;
 
+
+const firebaseConfig = {
+    apiKey: myKey,
+    authDomain: "aisturdyhelp.firebaseapp.com",
+    projectId: "aisturdyhelp",
+    storageBucket: "aisturdyhelp.appspot.com",
+    messagingSenderId: "309870602306",
+    appId: "1:309870602306:web:2273af72ef06e750c0a5db",
+    measurementId: "G-L12NERV1JR"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  
+  
+
+
 export default async function handler(req, res) {
-    if (req.method === 'GET') {
-        try {
-        
-            res.status(200).json({ myKey });
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    } else {
-        res.status(405).json({ error: 'Method not allowed' });
-    }
+
+
+
+// Add a new document in collection "cities"
+await setDoc(doc(db, "cities", "LA"), {
+  name: "Los Angeles",
+  state: "CA",
+  country: "USA"
+});
+
+
 }
