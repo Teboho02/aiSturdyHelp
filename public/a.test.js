a592e6971a46e5506f49fdb5cea907c8355ced6e
= <=>
2024-09-11 12:41:21 +0200

    dadwa

---
+++ public/a.test.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+
+describe('Simple Page Tests', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({ headless: true });
+    page = await browser.newPage();
+    await page.goto(`file://${__dirname}/a.html`);
+  });
+
+  afterAll(async () => {
+    if (browser) {
+      await browser.close();
+    }
+  });
+
+  test('Header should contain "Hello, World!"', async () => {
+    const headerText = await page.$eval('#header', el => el.textContent);
+    expect(headerText).toBe('Hello, World!');
+  });
+
+  test('Description should contain "This is a simple HTML page."', async () => {
+    const descriptionText = await page.$eval('#description', el => el.textContent);
+    expect(descriptionText).toBe('This is a simple HTML page.');
+  });
+});
